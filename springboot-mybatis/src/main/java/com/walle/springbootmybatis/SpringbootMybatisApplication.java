86aa5d37221a633d582690c2426996502c7224a7
wuyong <824991669@qq.com>
2024-01-27 00:48:23 +0800

    看看源码

--- springboot-mybatis/src/main/java/com/walle/springbootmybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/walle/springbootmybatis/SpringbootMybatisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootMybatisApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootMybatisApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+	}
 
 }
