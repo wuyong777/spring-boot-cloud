bf899c10d6eb85dd3db77be307c44767e34298d7
wuyong <824991669@qq.com>
2024-02-04 02:25:53 +0800

    update

--- Java8/src/com/walle/SE/PrimitiveTypeTest.java
+++ Java8/src/com/walle/SE/PrimitiveTypeTest.java
@@ -48,10 +48,9 @@ public class PrimitiveTypeTest {
         System.out.println("基本类型：char 二进制位数：" + Character.SIZE);
         System.out.println("包装类：java.lang.Character");
         // 以数值形式而不是字符形式将Character.MIN_VALUE输出到控制台
-        System.out.println("最小值：Character.MIN_VALUE="
-                + (int) Character.MIN_VALUE);
+        System.out.println("最小值：Character.MIN_VALUE=" + (int) Character.MIN_VALUE);
         // 以数值形式而不是字符形式将Character.MAX_VALUE输出到控制台
-        System.out.println("最大值：Character.MAX_VALUE="
-                + (int) Character.MAX_VALUE);
+        System.out.println("最大值：Character.MAX_VALUE=" + (int) Character.MAX_VALUE);
+
     }
 }
