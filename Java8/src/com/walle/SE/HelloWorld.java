874ce373d3792765b6209b8eb9ec59063ae143a4
wuyong <824991669@qq.com>
2024-02-19 12:40:39 +0800

    练习快捷键

---
+++ Java8/src/com/walle/SE/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.walle.SE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello");
+        System.out.println();
+
+        String s = new String("world");
+        List<String> strings = new ArrayList<>();
+        strings.add("aa");
+        strings.add("bb");
+        strings.add("cc");
+        for (String string : strings) {
+            if (string == null) {
+
+            }
+            if (string != null) {
+
+            }
+        }
+
+    }
+}
